0b1c439799a07f9ff6b9fb9140ef2ffb0ae9aac0
NeonJarbas <59943014+NeonJarbas@users.noreply.github.com>
2022-08-07 00:28:31 +0100

    feat/fuzzy (#1)
    allow more than exact matches via the new fuzz flag
    
    Co-authored-by: jarbasai <jarbasai@mailfence.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='padacioso',
-    version='0.1.1',
+    version='0.1.2',
     packages=['padacioso'],
     url='https://github.com/OpenJarbas/padacioso',
     license='apache-2.0',
