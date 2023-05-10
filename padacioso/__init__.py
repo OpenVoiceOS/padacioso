596dafc7d48f3be153f85248f1968d288dc89205
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2023-05-10 11:15:33 -0700

    Optimize intent matching (#10)
    * Update IntentContainer to keep `Matcher` objects instead of re-initializing at runtime
    
    * Move `Matcher` init to intent creation time
    
    * Sort intent regex at load instead of runtime
    
    * Add tests for intent/entity add/remove
    Raise exceptions when trying to re-register an existing intent/entity
    Remove unused `intents` and `entities` parameters in `IntentContainer`

--- padacioso/__init__.py
+++ padacioso/__init__.py
@@ -1,5 +1,6 @@
 import simplematch
 
+from typing import List, Iterator, Optional
 from padacioso.bracket_expansion import expand_parentheses, clean_braces
 
 try:
@@ -12,16 +13,19 @@ except ImportError:
 class IntentContainer:
     def __init__(self, fuzz=False):
         self.intent_samples, self.entity_samples = {}, {}
-        self.intents, self.entities = {}, {}
+        # self.intents, self.entities = {}, {}
         self.fuzz = fuzz
+        self._cased_matchers = dict()
+        self._uncased_matchers = dict()
 
-    def add_intent(self, name, lines):
-        expanded = []
-        for l in lines:
-            expanded += expand_parentheses(clean_braces(l))
-        self.intent_samples[name] = list(set(expanded))
-
-    def _get_fuzzed(self, sample):
+    @staticmethod
+    def _get_fuzzed(sample: str) -> List[str]:
+        """
+        Get fuzzy match examples by allowing a wildcard in place of each
+        specified word.
+        @param sample: Utterance example to mutate
+        @return: list of fuzzy string alternatives to `sample`
+        """
         fuzzed = []
         words = sample.split(" ")
         for idx in range(0, len(words)):
@@ -32,32 +36,81 @@ class IntentContainer:
             fuzzed.append(" ".join(new_words))
         return fuzzed + [f"* {sample}", f"{sample} *"]
 
-    def remove_intent(self, name):
+    def add_intent(self, name: str, lines: List[str]):
+        """
+        Add an intent with examples.
+        @param name: name of intent to add
+        @param lines: list of intent regexes
+        """
         if name in self.intent_samples:
-            del self.intent_samples[name]
+            raise RuntimeError(f"Attempted to re-register existing intent: "
+                               f"{name}")
+        expanded = []
+        for l in lines:
+            expanded += expand_parentheses(clean_braces(l))
+        regexes = list(set(expanded))
+        regexes.sort(key=len, reverse=True)
+        self.intent_samples[name] = regexes
+        for r in regexes:
+            self._cased_matchers[r] = \
+                simplematch.Matcher(r, case_sensitive=True)
+            self._uncased_matchers[r] = \
+                simplematch.Matcher(r, case_sensitive=False)
 
-    def add_entity(self, name, lines):
+    def remove_intent(self, name: str):
+        """
+        Remove an intent
+        @param name: name of intent to remove
+        """
+        if name in self.intent_samples:
+            regexes = self.intent_samples.pop(name)
+            for rx in regexes:
+                if rx in self._cased_matchers:
+                    self._cased_matchers.pop(rx)
+                if rx in self._uncased_matchers:
+                    self._uncased_matchers.pop(rx)
+
+    def add_entity(self, name: str, lines: List[str]):
+        """
+        Add an entity with examples.
+        @param name: name of entity to add
+        @param lines: list of entity examples
+        """
+        if name in self.entity_samples:
+            raise RuntimeError(f"Attempted to re-register existing entity: "
+                               f"{name}")
         name = name.lower()
         expanded = []
         for l in lines:
             expanded += expand_parentheses(l)
         self.entity_samples[name] = expanded
 
-    def remove_entity(self, name):
+    def remove_entity(self, name: str):
+        """
+        Remove an entity
+        @param name: name of entity to remove
+        """
         name = name.lower()
         if name in self.entity_samples:
             del self.entity_samples[name]
 
-    def calc_intents(self, query):
+    def calc_intents(self, query: str) -> Iterator[dict]:
+        """
+        Determine possible intents for a given query
+        @param query: input to evaluate for an intent match
+        @return: yields dict intent matches
+        """
         for intent_name, regexes in self.intent_samples.items():
-            regexes = sorted(regexes, key=len, reverse=True)
             for r in regexes:
                 penalty = 0
                 if "*" in r:
                     # penalize wildcards
                     penalty = 0.15
-
-                entities = simplematch.match(r, query, case_sensitive=True)
+                if r not in self._cased_matchers:
+                    LOG.warning(f"{r} not initialized")
+                    self._cased_matchers[r] = \
+                        simplematch.Matcher(r, case_sensitive=True)
+                entities = self._cased_matchers[r].match(query)
                 if entities is not None:
                     for k, v in entities.items():
                         if k not in self.entity_samples:
@@ -71,7 +124,11 @@ class IntentContainer:
                            "name": intent_name}
                     break
 
-                entities = simplematch.match(r, query, case_sensitive=False)
+                if r not in self._uncased_matchers:
+                    LOG.warning(f"{r} not initialized")
+                    self._uncased_matchers[r] = \
+                        simplematch.Matcher(r, case_sensitive=False)
+                entities = self._uncased_matchers[r].match(query)
                 if entities is not None:
                     # penalize case mismatch
                     penalty += 0.05
@@ -88,6 +145,7 @@ class IntentContainer:
                     break
 
                 if self.fuzz:
+                    LOG.debug(f"Fallback to fuzzy match")
                     penalty += 0.25
                     for f in self._get_fuzzed(r):
                         entities = simplematch.match(f, query,
@@ -98,7 +156,12 @@ class IntentContainer:
                                    "name": intent_name}
                             break
 
-    def calc_intent(self, query):
+    def calc_intent(self, query: str) -> Optional[dict]:
+        """
+        Determine the best intent match for a given query
+        @param query: input to evaluate for an intent
+        @return: dict matched intent (or None)
+        """
         match = max(
             self.calc_intents(query),
             key=lambda x: x["conf"],
